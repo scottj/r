d69dff51c89c38044bfdc1b3dc1e8fb46ea57a50
Scott Johnson <scottj@users.noreply.github.com>
2026-04-17 16:52:08 -0500

    add deployment workflow and minification

---
+++ scripts/minify.ps1
@@ -0,0 +1,28 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+Push-Location "$PSScriptRoot/.."
+try {
+    bunx html-minifier-terser `
+        --collapse-boolean-attributes `
+        --collapse-whitespace `
+        --decode-entities `
+        --minify-css true `
+        --minify-js true `
+        --remove-attribute-quotes `
+        --remove-comments `
+        --remove-empty-attributes `
+        --remove-redundant-attributes `
+        --remove-script-type-attributes `
+        --remove-style-link-type-attributes `
+        --sort-attributes `
+        --sort-class-name `
+        --trim-custom-fragments `
+        --use-short-doctype `
+        --process-conditional-comments `
+        --remove-optional-tags `
+        --no-include-auto-generated-tags `
+        -o index.html `
+        src/index.html
+} finally {
+    Pop-Location
+}
